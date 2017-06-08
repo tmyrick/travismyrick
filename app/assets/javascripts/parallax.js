90ceed60a72c37354620e2904726824ba0dcb8bb
tmyrick <travis.myrick@getg5.com>
2017-06-07 22:40:01 -0700

    add assets, add styles for parallax and sticky nav, add jquery for bottom to top sticky nav

--- app/assets/javascripts/parallax.js
+++ app/assets/javascripts/parallax.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+
+$(document).ready(function(){
+  $(window).scroll(function () {
+    var viewport = $(window).height();
+    var scrolltop = ($(window).scrollTop())
+      if ($(window).scrollTop() > viewport - 64) {
+       $('header').addClass('fixed');
+       $('.container').addClass('add-pad')
+      }
+      else {
+        $('header').removeClass('fixed');
+        $('.container').removeClass('add-pad')
+      }
+
+    $('.hideme').each( function(i){
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+        $(this).animate({'opacity':'1'},800);
+      }
+    });
+  })
+});
+
+
